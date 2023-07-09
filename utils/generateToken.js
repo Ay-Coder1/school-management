a2d23f315a4538cbacb6fe5598ea710f0708cdbd
smarts8855 <85696168+smarts8855@users.noreply.github.com>
2023-07-09 15:36:16 +0100

    dbconnect

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
-
+const secret = process.env.secret;
 const generateToken = (id) => {
-  return jwt.sign({ id }, "anykey", { expiresIn: "5d" });
+  return jwt.sign({ id }, secret, { expiresIn: "5d" });
 };
 
 module.exports = generateToken;
