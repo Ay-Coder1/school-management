23b3538ca91aa2175438fffe847443c5ffa0677e
Ayomide <ayomidebabarinde07@gmail.com>
2023-10-31 03:40:06 -0700

    first commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require("./config/dbConnect");
 const app = require("./app/app");
 
 const PORT = process.env.PORT || 2020;
-
+    
 //server
 const server = http.createServer(app);
 server.listen(PORT, console.log(`Server is running on port ${PORT}`));
